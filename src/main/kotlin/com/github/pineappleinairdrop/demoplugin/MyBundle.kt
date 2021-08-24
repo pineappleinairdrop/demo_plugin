1750b9bbbdeab47378bcb22a0cd43b62f4f87716
GitHub Action <action@github.com>
2021-08-24 06:32:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/pineappleinairdrop/demoplugin/MyBundle.kt
+++ src/main/kotlin/com/github/pineappleinairdrop/demoplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pineappleinairdrop.demoplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/pineappleinairdrop/demoplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pineappleinairdrop.demoplugin.services
+
+import com.github.pineappleinairdrop.demoplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
